6c27f357199a118329096d7ff631eff5ac628ed7
Azizener <116525186+azizener@users.noreply.github.com>
2023-09-27 06:00:44 +0300

    Add files via upload

--- Python102.py
+++ Python102.py
@@ -1,38 +1,21 @@
-import datetime
+phone_directory = {
+    "Amal": "1111111111",
+    "Mohammed": "2222222222",
+    "Khadijah": "3333333333",
+    "Abdullah": "4444444444",
+    "Rawan": "5555555555",
+    "Faisal": "6666666666",
+    "Layla": "7777777777"
+}
 
-name1 = "khalid"
-date1 = datetime.date(1989,2,1)
-name2 = "Nouf"
-date2 = datetime.date(2004,9,2)
-name3 = "khalid"
-date3 = datetime.date(2009,12,9)
-age1 = 2021-date1.year
-day1 = date1.strftime("%A")
-print(name1 + " is " + str(age1) + " years old and he/she was born on " + day1)
-age2 = 2021-date2.year
-day2 = date2.strftime("%A")
-print(name2 + " is " + str(age2) + " years old and he/she was born on " + day2)
-age3 = 2021-date3.year
-day3 = date3.strftime("%A")
-print(name3 + " is " + str(age3) + " years old and he/she was born on " + day3)
+user_input = input("Enter a phone number: ")
 
-ages = [age1, age2, age3]
-
-Max = max(ages)
-Min = min(ages)
-
-if Max == age1:
-    print("The oldest one is "+ str(age1))
-elif Max == age2:
-    print("The oldest one is " + str(age2))
+if not user_input.isdigit() or len(user_input) != 10:
+    print("This is an invalid number.")
 else:
-    print("The oldest one is " + str(age3))
+    name = phone_directory.get(user_input, None)
 
-if Min == age1:
-    print("The youngest one " + str(age1))
-elif Min == age2:
-    print("The youngest one " + str(age2))
-else:
-    print("The youngest one " + str(age3))
-
-print("Total People: " + str(len(ages)))
+    if name:
+        print("Name:", name)
+    else:
+        print("Sorry, the number is not found.")
